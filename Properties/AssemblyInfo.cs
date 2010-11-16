ffe6206572931f6e74e39a69841a2331c158d48f
Jorn Skaarud Karlsen <jornskaa@gmail.com>
2010-11-16 20:12:36 +0100

    Added project files

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("dcm2dds")]
+[assembly: AssemblyDescription("Converts from DICOM images to XML, raw volume and DDS volume files and metadata.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jørn Skaarud Karlsen")]
+[assembly: AssemblyProduct("dcm2dds")]
+[assembly: AssemblyCopyright("Jørn Skaarud Karlsen")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4aa98c41-8994-4a6d-950d-18f935ae6c50")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
